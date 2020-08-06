41348225ae133c6c55c79d095f9cf47bf04f5c16
TalhaAzim <talha.azim@gmail.com>
2020-08-06 00:50:09 -0400

    No clear direction yet

--- default/main.py
+++ default/main.py
@@ -1,36 +1,58 @@
 from flask import Flask
 import json
-from google.cloud import firestore
+from immutables import Map
 
 app = Flask(__name__)
 
-def get_published_posts():
-    db = firestore.Client()
-    posts = db.collection("posts").where("published", "==", True).order_by("date_published", direction=firestore.Query.DESCENDING).limit(10).stream()
-    return [post.to_dict() for post in posts]
+class Model(object):
 
-def render_post(post):
-    return "".join([
-        "<article>",
-        f'<header><h1>{post["title"]} by {post["author"]["name"]}</h1></header>',
-        f'<div>{post["body"]}</div>',
-        "</article>"
-    ])
+    from google.cloud import firestore as fs
+    db = fs.Client()
+
+class Post(Model):
+
+    collection = "posts"
+    
+    @classmethod
+    def get_published(cls, limit=10):
+        return cls.db.collection(cls.collection)\
+            .where("published", "==", True)\
+            .order_by("date_published", direction=cls.fs.Query.DESCENDING)\
+            .limit(limit)\
+            .stream()
+
+
+@app.route("/api/published_posts")
+def published_posts():
+    posts = Post.get_published()
+    return json.dumps(
+        [ {'id': post.id, **post.to_dict()} for post in posts],
+        default=str
+    )
 
 @app.route("/")
 def index():
-    posts = get_published_posts()
+    posts = Post.get_published()
     return "".join([
         "<html>",
         "<head>",
         "<title>Solid Blog Simulation</title>",
-        "<script type=\"text/javascript\">",
-        f'var posts={json.dumps(posts, default=str)}'
-        "</script>"
+        "<script src=\"https://cdn.jsdelivr.net/npm/vue@2/dist/vue.min.js\"></script>"
         "</head>",
         "<body>",
         "<header><h1>Solid Blog Simulation</h1></header>",
-        "".join([render_post(post) for post in posts]),
+        "<div id=\"app\">",
+        "<article v-bind:id=\"post.id\" v-for=\"post of posts\">",
+        "<header><h1>{{ post.title }} by {{ post.author.name }}</h1></header>",
+        "<div v-html=\"post.body\"></div>",
+        "</article>",
+        "</div>",
+        "<script type=\"text/javascript\">",
+        f'var app = new Vue({{'
+        f'el: "#app",'
+        f'data: {{ posts: {json.dumps([{"id": post.id, **post.to_dict()} for post in posts], default=str)}}}'
+        f'}})',
+        "</script>",
         "</body>",
         "</html>"
     ])
